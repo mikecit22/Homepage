0dbc355f9377985fcf6dbe0520e40cf661c6cc74
mikecit22 <you@example.com>
2015-10-27 17:42:18 -0400

    c

--- views/technologies.php
+++ views/technologies.php
@@ -8,53 +8,52 @@
 </div>
 <div class="container">
     <div class="row">
-        <div class="col-lg-3 col-md-3 col-xs-12 text-center">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-8 col-xs-offset-2 text-center">
             <div class="service-box service-box-mod">
                 <i class="icon-java-bold languages-icon" style="margin-left:40px;"></i>
                 <h3>JAVA</h3>
             </div>
         </div>
-        <div class="col-lg-3 col-md-3 col-xs-12 text-center">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-8 col-xs-offset-2 text-center">
             <div class="service-box service-box-mod">
                 <i class="icon-php languages-icon"></i>
                 <h3>PHP</h3>
                 
             </div>
         </div>
-        <div class="col-lg-3 col-md-3 col-xs-12 text-center">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-8 col-xs-offset-2 text-center">
             <div class="service-box service-box-mod">
                 <i class="icon-mysql languages-icon"></i>
                 <h3>MySQL</h3>
             </div>
         </div>
-        <div class="col-lg-3 col-md-3 col-xs-12 text-center">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-8 col-xs-offset-2 text-center">
             <div class="service-box service-box-mod">
                 <i class="languages-icon"></i>
                 <h3>ColdFusion</h3>
             </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-lg-3 col-md-3 col-xs-12 text-center">
+
+        <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-8 col-xs-offset-2 text-center">
             <div class="service-box service-box-mod">
                 <i class="fa fa-html5 languages-icon"></i>
                 <h3>HTML5</h3>
             </div>
         </div>
-        <div class="col-lg-3 col-md-3 col-xs-12 text-center">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-8 col-xs-offset-2 text-center">
             <div class="service-box service-box-mod">
                 <i class="fa fa-css3 languages-icon"></i>
                 <h3>CSS3</h3>
                 
             </div>
         </div>
-        <div class="col-lg-3 col-md-3 col-xs-12 text-center">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-8 col-xs-offset-2 text-center">
             <div class="service-box service-box-mod">
                 <i class="icon-javascript languages-icon"></i>
                 <h3>JavaScript</h3>
             </div>
         </div>
-        <div class="col-lg-3 col-md-3 col-xs-12 text-center">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-8 col-xs-offset-2 text-center">
             <div class="service-box service-box-mod">
                 <i class="fa fa-file-excel-o languages-icon" style="margin-top: 40px;"></i>
                 <h3>XML</h3>
@@ -73,53 +72,51 @@
 </div>
 <div class="container">
     <div class="row">
-        <div class="col-lg-3 col-md-3 col-xs-12 text-center">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-8 col-xs-offset-2 text-center">
             <div class="service-box service-box-mod">
                 <i class="fa fa-android languages-icon" style="margin-top: 40px;"></i>
                 <h3>Android</h3>
             </div>
         </div>
-        <div class="col-lg-3 col-md-3 col-xs-12 text-center">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-8 col-xs-offset-2 text-center">
             <div class="service-box service-box-mod">
                 <i class="fa fa-apple languages-icon" style="margin-top: 40px;"></i>
                 <h3>iOS</h3>
                 
             </div>
         </div>
-        <div class="col-lg-3 col-md-3 col-xs-12 text-center">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-8 col-xs-offset-2 text-center">
             <div class="service-box service-box-mod">
                 <i class="fa fa-windows languages-icon" style="margin-top: 40px;"></i>
                 <h3>Windows</h3>
             </div>
         </div>
-        <div class="col-lg-3 col-md-3 col-xs-12 text-center">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-8 col-xs-offset-2 text-center">
             <div class="service-box service-box-mod">
                 <i class="fa fa-apple languages-icon" style="margin-top: 40px;"></i>
                 <h3>Mac</h3>
             </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-lg-3 col-md-3 col-xs-12 text-center">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-8 col-xs-offset-2 text-center">
             <div class="service-box service-box-mod">
                 <i class="icon-html languages-icon" style="margin-left: 35px;"></i>
                 <h3>web</h3>
             </div>
         </div>
-        <div class="col-lg-3 col-md-3 col-xs-12 text-center">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-8 col-xs-offset-2 text-center">
             <div class="service-box service-box-mod">
                 <i class="icon-raspberrypi languages-icon" style="margin-left: 40px;"></i>
                 <h3>raspberry pi</h3>
                 
             </div>
         </div>
-        <div class="col-lg-3 col-md-3 col-xs-12 text-center">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-8 col-xs-offset-2 text-center">
             <div class="service-box service-box-mod">
                 <i class="icon-ubuntu languages-icon"></i>
                 <h3>linux</h3>
             </div>
         </div>
-        <div class="col-lg-3 col-md-3 col-xs-12 text-center">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-8 col-xs-offset-2 text-center">
             <div class="service-box service-box-mod">
                 <i class="icon-apache languages-icon"></i>
                 <h3>apache</h3>
@@ -138,26 +135,26 @@
 </div>
 <div class="container">
     <div class="row">
-        <div class="col-lg-3 col-md-3 col-xs-12 text-center">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-8 col-xs-offset-2 text-center">
             <div class="service-box service-box-mod">
                 <i class="icon-laravel languages-icon"></i>
                 <h3>Laravel</h3>
             </div>
         </div>
-        <div class="col-lg-3 col-md-3 col-xs-12 text-center">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-8 col-xs-offset-2 text-center">
             <div class="service-box service-box-mod">
                 <i class="php languages-icon"></i>
                 <h3>Ionic</h3>
                 
             </div>
         </div>
-        <div class="col-lg-3 col-md-3 col-xs-12 text-center">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-8 col-xs-offset-2 text-center">
             <div class="service-box service-box-mod">
                 <i class="mysql languages-icon"></i>
                 <h3>Bootstrap</h3>
             </div>
         </div>
-        <div class="col-lg-3 col-md-3 col-xs-12 text-center">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-sm-offset-0 col-xs-8 col-xs-offset-2 text-center">
             <div class="service-box service-box-mod">
                 <i class="c languages-icon"></i>
                 <h3>Much More</h3>
