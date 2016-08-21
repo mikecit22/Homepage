22e6c8289fa6ad2d3d16199546f79d6297f5d6ab
Michael Citro <mcitro@MichaelCitroMBP.home>
2016-08-21 10:19:23 -0400

    updates

--- blog/index.php
+++ blog/index.php
@@ -24,21 +24,16 @@
 	<link rel="stylesheet" type="text/css" href="lib/semantic/semantic.min.css">
 </head>
 <body>
-	<div class="ui pointing menu">
+	<div class="ui pointing menu sticky">
 	  <a class="active item">
 	    Michael Citro' Blog
 	  </a>
-	  <a class="item" style="border:none;">
-	    	<div class="fb-like" data-href="https://citrosystems.com/Homepage/blog" data-layout="button_count" data-action="like" data-size="large" data-show-faces="true" data-share="true" ></div>
-	  </a>
-<!-- 	  <div class="right menu">
+	  <div class="right menu">
 	    <div class="item">
-	      <div class="ui transparent icon input">
-	        <input type="text" placeholder="Search...">
-	        <i class="search link icon"></i>
+ 			<div class="fb-like" data-href="https://citrosystems.com/Homepage/blog" data-layout="button_count" data-action="like" data-size="large" data-show-faces="true" data-share="true" ></div>
 	      </div>
 	    </div>
-	  </div> -->
+	  </div>
 	</div>
 	<div class="ui centered grid">
 		<div class="sixteen wide column">
@@ -60,23 +55,38 @@
 			<h2>Welcome, please read this intro to get a better understanding of what topices i will be covering, Thank You.</h2>
 		</div>
 		<div class="sixteen wide column">
-			<h3 class="ui horizontal divider header">
+			<h1 class="ui horizontal divider header">
 			  Introduction
-			</h3>
+			</h1>
+		</div>
+		<div class="sixteen wide column" style="padding: 0px 35px 0px 35px;">
+			<p style="text-align: left;">
+				Hi and welcome to my blog. In my initial few postings I will be getting into the basics of web development making my way from frontend developer to backend development and covering a few different technologies, I will explain what Frontend vs. Backend is don’t worry. I will be mainly aiming to aid those trying to get into web development but get lost in all the acronyms and terminologies that clutter tutorials now adays. There are an abundant of new frameworks, I will explain what frameworks are as well, but the underlying technologies are all the same. It’s these technologies that you should master in order to be able to take full advantaged and truly appreciate these technologies, my opinion. As a new web developer, or just want to educate yourself on web development, if you get caught up trying to start off using these frameworks and not truly understanding the underlying technologies that they are built on you will have many gaps in your knowledge, and therefor giving you a disadvantage. 
+			</p>
+			<p style="text-align: left;">
+				I will not be going to details or getting to tecknogiesly involved initialy but will give a genial overview of how web developer works and it all connects and works. I truly belive that knowing the archecutre of something you are tyring to learned it the best starting point. Followed by leaneing the underlying tecknlogies that it is bulilt on. Only then can you start to take advantaged of these advanced new framworks. I feel it a misconception, again my opiion, that you must take classes to be a great developer. My opion is that it does not take classes but just passion detemineation and motivation to read, implement, and learn. Also please not that in all my blogs these are my opions based off of many years of reseach and work/personal experance. If you do not agree with any of my options im sorry, but for the post part I will be explaining how web tecknoges/devlopemnt works, which are facts. Whither or not I approve or like one tecbknlogoe over another is were my opione will come in, but that should be up to you to decied by implementing and practiving it. I am a great advocated of teaching others to code becusing I feel that this forced a diferant way of thinking and vewing the world, so now lets begin 
+
+				PS: Please comment below if you have an opion on something I said, or somhting I said is incoorect or acincaacure, or just want more clarity on something I said
+			</p>			
+		</p>
 		</div>
 	</div>
 
 	<footer>
+		<div class="ui footer" style="margin-top:15px;">
+			<div class="fb-comments" data-href="https://citrosystems.com/Homepage/blog" data-numposts="5"></div>
+			<p style="text-align:center;"><small>&copy; 2016. Michael Citro. All Rights Reserverd.</small></p>
+		</div>
 		<div class="fb-buttons">
-			<script src="lib/jquery.min.js"></script>
-			<script src="lib/semantic/semantic.min.js"></script>
+		<script src="lib/jquery.min.js"></script>
+		<script src="lib/semantic/semantic.min.js"></script>
 
-			<script>(function(d, s, id) {
-				var js, fjs = d.getElementsByTagName(s)[0];
-				if (d.getElementById(id)) return;
-				js = d.createElement(s); js.id = id;
-				js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7";
-				fjs.parentNode.insertBefore(js, fjs);
+		<script>(function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s); js.id = id;
+			js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7";
+			fjs.parentNode.insertBefore(js, fjs);
 			}(document, 'script', 'facebook-jssdk'));
 		</script>
 
@@ -98,6 +108,15 @@
 
 			ga('create', 'UA-42972594-1', 'auto');
 			ga('send', 'pageview');
+		</script>
+
+		<script type="text/javascript">
+			$(document).ready(function(){
+				$('.ui.sticky.menu')
+				  .sticky({
+				    context: 'body'
+				  });
+			})
 		</script>
 	</footer>
 </body>
