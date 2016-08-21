bbdf4a7c4c2dd675ee76e1c3143d680f73aa3ff3
Michael Citro <mcitro@MichaelCitroMBP.home>
2016-08-21 10:01:49 -0400

    updates

---
+++ blog/index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Citro's Blog</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Citro Systems Blog Posting" />
+	<meta name="keywords" content="michael citro, citro blog, blog posting" />
+	<meta name="author" content="Michael Citro" />
+
+	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link rel="shortcut icon" href="favicon.ico">
+	<link rel="stylesheet" type="text/css" href="lib/semantic/semantic.min.css">
+</head>
+<body>
+	<div class="ui pointing menu">
+	  <a class="active item">
+	    Michael Citro' Blog
+	  </a>
+	  <a class="item" style="border:none;">
+	    	<div class="fb-like" data-href="https://citrosystems.com/Homepage/blog" data-layout="button_count" data-action="like" data-size="large" data-show-faces="true" data-share="true" ></div>
+	  </a>
+<!-- 	  <div class="right menu">
+	    <div class="item">
+	      <div class="ui transparent icon input">
+	        <input type="text" placeholder="Search...">
+	        <i class="search link icon"></i>
+	      </div>
+	    </div>
+	  </div> -->
+	</div>
+	<div class="ui centered grid">
+		<div class="sixteen wide column">
+			<div class="ui link cards centered">
+				<div class="card">
+					<div class="image">
+						<img src="../img/pp.jpg">
+					</div>
+					<div class="content">
+						<div class="header">Michael Citro' Blog</div>
+						<div class="description">
+							Updated on August 20, 2016
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="sixteen wide column">
+			<h2>Welcome, please read this intro to get a better understanding of what topices i will be covering, Thank You.</h2>
+		</div>
+		<div class="sixteen wide column">
+			<h3 class="ui horizontal divider header">
+			  Introduction
+			</h3>
+		</div>
+	</div>
+
+	<footer>
+		<div class="fb-buttons">
+			<script src="lib/jquery.min.js"></script>
+			<script src="lib/semantic/semantic.min.js"></script>
+
+			<script>(function(d, s, id) {
+				var js, fjs = d.getElementsByTagName(s)[0];
+				if (d.getElementById(id)) return;
+				js = d.createElement(s); js.id = id;
+				js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7";
+				fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));
+		</script>
+
+		<!-- Facebook Comments -->
+		<script>(function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s); js.id = id;
+			js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
+
+		<!-- Google Anily -->
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-42972594-1', 'auto');
+			ga('send', 'pageview');
+		</script>
+	</footer>
+</body>
+</html>
