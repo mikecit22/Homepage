29b148393c0fee44653c94849f6337db61c411cf
Michael Citro <mcitro@MichaelCitroMBP.home>
2016-08-27 22:17:41 -0400

    update

--- blog/partials/postset1.php
+++ blog/partials/postset1.php
@@ -20,10 +20,12 @@
 	    <div class="content">
 	      <a class="header">The Web Browser</a>
 	      <div class="meta">
-	        <span>Also known as Front end or client side this is all the code that runs on the broswer. I will review this as HTML css JAvscript being th e fundation of Frontend Technologies </span>
+	        <span style="line-height: 1.58; letter-spacing: -.003em;">
+				Writing code that runs on the browser is known as Frontend development, also referred to as Client side development. This is because code that is written by the developers is ran completely on an individual user's browser, may it be Safari, Firefox, Internet Explorer, phone, tablet, or any other web browser. In this post, I will review the fundamental technologies involved in Frontend development, which are HTML, CSS, and Javascript.
+	        </span>
 	      </div>
 	      <div class="extra">
-	        Ahust 20th, 2016
+	        September 10th, 2016
 	      </div>
 	    </div>
 	  </div>
@@ -41,9 +43,11 @@
 			</div>
 	    </div>
 	    <div class="content">
-	      <a class="header">How Data Gets Onto Your Browser</a>
+	      <a class="header">Data's Jurney To The Browser.</a>
 	      <div class="meta">
-	        <span>Also known as Front end or client side this is all the code that runs on the broswer. I will review this as HTML css JAvscript being th e fundation of Frontend Technologies </span>
+	        <span style="line-height: 1.58; letter-spacing: -.003em;">
+				Now that you are familiar with: how elements are displayed on the page, how they are styled, and how they are given functionality to perform tasks, it's time to talk about how data, such as user information, is populated on the site. An example is, how do all those Facebook posts get displayed in your news feed, they must be coming from somewhere, right?
+	        </span>
 	      </div>
 	      <div class="extra">
 	        Coming Soon!
