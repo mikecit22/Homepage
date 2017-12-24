08c6a397aa210010eb4d411ad65922980404191b
mikecit22 <Mike@Ishwaryas-MacBook-Pro.local>
2017-12-24 13:44:36 -0500

    portfolio updates

--- views/portfolio.php
+++ views/portfolio.php
@@ -7,6 +7,20 @@
     </div>
     <div class="row">
         <div class="col-md-4 col-sm-6 portfolio-item">
+            <a href="https://www.childrensplace.com" class="portfolio-link" data-toggle="modal">
+                <div class="portfolio-hover">
+                    <div class="portfolio-hover-content">
+                        <i class="fa fa-link " style="font-size:60px;"></i>
+                    </div>
+                </div>
+                <img src="img/portfolio/andy.png" class="img-responsive" alt="">
+            </a>
+            <div class="portfolio-caption">
+                <h4>The Children's Place</h4>
+                <p class="text-muted">Lead the Nodejs, React/Redux Development Efforts</p>
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-6 portfolio-item">
             <a href="http://alcontractingllc.com" class="portfolio-link" data-toggle="modal">
                 <div class="portfolio-hover">
                     <div class="portfolio-hover-content">
