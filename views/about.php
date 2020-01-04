2fc96c8452ffe06ac24edf53325027debf0b52f8
mikecit22 <mikecit22@gmail.com>
2020-01-04 10:09:42 -0500

    updated work history

--- views/about.php
+++ views/about.php
@@ -2,65 +2,65 @@
             <div class="row">
                 <div class="col-lg-12 text-center">
                     <h2 class="section-heading">About</h2>
-                    <h3 class="section-subheading text-muted" style="line-height: 30px;">As a Software Engineering I have held positions from a Systems Architect Analyst to a front and back-end developer in both corporate and startup environments. Aside from completing projects working with different teams across different corporations, I have worked with numerous complex systems both from an architectural standpoint and development standpoint. I invest personal time experimenting with new and emerging technologies to better myself in designing and implementing systems, not only to improve my skill but because what I do is my life’s passion.</h3>
+                    <h3 class="section-subheading text-muted" style="line-height: 30px;">As a Software Engineer I have held positions from a Systems Architect Analyst to a front and back-end developer in both corporate and startup environments. Aside from completing projects working with different teams across different corporations, I have worked with numerous complex systems both from an architectural standpoint and development standpoint. I invest personal time experimenting with new and emerging technologies to better myself in designing and implementing systems, not only to improve my skill but because what I do is my life’s passion.</h3>
                 </div>
             </div>
             <div class="row">
                 <div class="col-lg-12">
                     <ul class="timeline">
-                        <li>
+                        <li class="">
                             <div class="timeline-image">
-                                <img class="img-circle img-responsive" src="img/about/gs.jpg" alt="">
+                                <img class="img-circle img-responsive" src="img/about/cbi.jpg" alt="">
                             </div>
                             <div class="timeline-panel">
                                 <div class="timeline-heading">
-                                    <h4>2011-2013</h4>
-                                    <h4 class="subheading">Geek Squad</h4>
+                                    <h4>Current</h4>
+                                    <h4 class="subheading">CB Insights</h4>
                                 </div>
                                 <div class="timeline-body">
-                                    <p class="text-muted">Advanced Repair Agent</p>
+                                    <p class="text-muted">Senior Software Engineer</p>
                                 </div>
                             </div>
                         </li>
                         <li class="timeline-inverted">
                             <div class="timeline-image">
-                                <img class="img-circle img-responsive" src="img/about/io.jpg" alt="">
+                                <img class="img-circle img-responsive" src="img/about/tcp2.jpg" alt="">
                             </div>
                             <div class="timeline-panel">
                                 <div class="timeline-heading">
-                                    <h4>2013</h4>
-                                    <h4 class="subheading">Develop.io</h4>
+                                    <h4>2018</h4>
+                                    <h4 class="subheading">The Children's Place</h4>
                                 </div>
                                 <div class="timeline-body">
-                                    <p class="text-muted">Web Developer</p>
+                                    <p class="text-muted">Lead Software Engineer</p>
                                 </div>
                             </div>
                         </li>
-                        <li>
+                        <li class="">
                             <div class="timeline-image">
-                                <img class="img-circle img-responsive" src="img/about/e.jpg" alt="">
+                                <img class="img-circle img-responsive" src="img/about/tcp2.jpg" alt="">
                             </div>
                             <div class="timeline-panel">
                                 <div class="timeline-heading">
-                                    <h4>2014</h4>
-                                    <h4 class="subheading">Ericsson</h4>
+                                    <h4>2017</h4>
+                                    <h4 class="subheading">The Children's Place</h4>
                                 </div>
                                 <div class="timeline-body">
-                                    <p class="text-muted">Cloud Manager Intern</p>
+                                    <p class="text-muted">Software Developer</p>
                                 </div>
                             </div>
                         </li>
                         <li class="timeline-inverted">
                             <div class="timeline-image">
-                                <img class="img-circle img-responsive" src="img/about/njit.jpg" alt="">
+                                <img class="img-circle img-responsive" src="img/about/tcp2.jpg" alt="">
                             </div>
                             <div class="timeline-panel">
                                 <div class="timeline-heading">
-                                    <h4>2014</h4>
-                                    <h4 class="subheading">New Jersey Institute of Technology</h4>
+                                    <h4>2016</h4>
+                                    <h4 class="subheading">The Children's Place</h4>
                                 </div>
                                 <div class="timeline-body">
-                                    <p class="text-muted">Research Assistant</p>
+                                    <p class="text-muted">Systems Architect Analyst</p>
                                 </div>
                             </div>
                         </li>
@@ -80,15 +80,57 @@
                         </li>
                         <li class="timeline-inverted">
                             <div class="timeline-image">
-                                <img class="img-circle img-responsive" src="img/about/tcp2.jpg" alt="">
+                                <img class="img-circle img-responsive" src="img/about/njit.jpg" alt="">
                             </div>
                             <div class="timeline-panel">
                                 <div class="timeline-heading">
-                                    <h4>Current</h4>
-                                    <h4 class="subheading">The Children's Place</h4>
+                                    <h4>2014</h4>
+                                    <h4 class="subheading">New Jersey Institute of Technology</h4>
                                 </div>
                                 <div class="timeline-body">
-                                    <p class="text-muted">Systems Architect Analyst</p>
+                                    <p class="text-muted">Research Assistant</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="timeline-image">
+                                <img class="img-circle img-responsive" src="img/about/e.jpg" alt="">
+                            </div>
+                            <div class="timeline-panel">
+                                <div class="timeline-heading">
+                                    <h4>2014</h4>
+                                    <h4 class="subheading">Ericsson</h4>
+                                </div>
+                                <div class="timeline-body">
+                                    <p class="text-muted">Cloud Manager Intern</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="timeline-inverted">
+                            <div class="timeline-image">
+                                <img class="img-circle img-responsive" src="img/about/io.jpg" alt="">
+                            </div>
+                            <div class="timeline-panel">
+                                <div class="timeline-heading">
+                                    <h4>2013</h4>
+                                    <h4 class="subheading">Develop.io</h4>
+                                </div>
+                                <div class="timeline-body">
+                                    <p class="text-muted">Web Developer</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="timeline-image">
+                                <img class="img-circle img-responsive" src="img/about/gs.jpg" alt="">
+                            </div>
+                            <div class="timeline-panel">
+                                <div class="timeline-heading">
+                                    <h4>2011-2013</h4>
+                                    <h4 class="subheading">Geek Squad</h4>
+                                </div>
+                                <div class="timeline-body">
+                                    <p class="text-muted">Advanced Repair Agent</p>
                                 </div>
                             </div>
                         </li>
